4e5b72df2111a3add929f17d21164fed478c585a
utahphotohunter <mitch.hunter1197@gmail.com>
2024-01-13 22:41:12 -0700

    prep 1 and prep 2 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        String lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your percentage in the class? ");
+        string grade = Console.ReadLine();
+        float numberGrade = float.Parse(grade);
+        
+        string letter = "";
+
+        if (numberGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (numberGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (numberGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (numberGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        if (letter == "F" || letter == "D")
+        {
+            Console.WriteLine($"Unfortunatley, you received a {letter} and did not pass the class. Consider studying harder next attempt.");
+        }
+        else if (letter == "A")
+        {
+            Console.WriteLine($"You passed the class with an {letter}!");
+        }
+        else
+        {
+            Console.WriteLine($"You passed the class with {letter}!");
+        }
+        
+        
     }
 }
