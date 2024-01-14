2df77dd18f465c3cde3f5998345ea9cbc35bb4a9
utahphotohunter <mitch.hunter1197@gmail.com>
2024-01-13 22:57:07 -0700

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+
+        Random randomNumber = new Random();
+        int number = randomNumber.Next(1,100);
+        Console.WriteLine("I am thinking of a number between 1 and 100.");
+        int finalGuess = 0;
+        while (finalGuess != number)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            int guessedNumber = int.Parse(guess);
+            finalGuess = guessedNumber;
+            if (guessedNumber < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessedNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
