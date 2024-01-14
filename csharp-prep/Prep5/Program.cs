485193ccb53d29b49981394f73844d396fcd1928
utahphotohunter <mitch.hunter1197@gmail.com>
2024-01-14 00:39:30 -0700

    finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        float userNumber = PromptUserNumber();
+        double squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber);
+    }
+
+
+
+
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static float PromptUserNumber()
+    {
+        Console.Write("What is your favorite Number? ");
+        string userNumber = Console.ReadLine();
+        float number = float.Parse(userNumber);
+        return number;
+    }
+    static double SquareNumber(float number)
+    {
+        double result = Math.Pow(number, 2);
+        return result;
+    }
+    static void DisplayResult(string userName, double squaredNumber)
+    {
+        string textVersion = squaredNumber.ToString();
+        Console.Write($"{userName} your number squared is {textVersion}.");
     }
 }
