b797034ea77ff7a40ebea58d9f5786deec743dbe
utahphotohunter <mitch.hunter1197@gmail.com>
2024-01-31 21:35:32 -0700

    added more code to activity class

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -16,9 +16,19 @@ class Activity
         _name = name;
     }
 
+    public string GetName()
+    {
+        return _name;
+    }
+
     public void SetDescription(string description)
     {
         _description = description;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
     }
 
     public void SetOpenMessage(string openMessage)
@@ -26,14 +36,29 @@ class Activity
         _openMessage = openMessage;
     }
 
+    public void DisplayOpenMessage()
+    {
+        Console.WriteLine(_openMessage);
+    }
+
     public void SetEndMessage(string endMessage)
     {
         _endMessage = endMessage;
     }
 
+    public void DisplayEndMessage()
+    {
+        Console.WriteLine(_endMessage);
+    }
+
     public void SetDuration(int duration)
     {
         _duration = duration;
+    }
+
+    public int GetDuration()
+    {
+        return _duration;
     }
 
 
