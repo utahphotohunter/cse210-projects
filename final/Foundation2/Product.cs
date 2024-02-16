bbcd7aeb079fb821602b47d32a1b3323462da813
utahphotohunter <mitch.hunter1197@gmail.com>
2024-02-16 13:53:29 -0700

    added member variables to Product class and added getters and setters.

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -1,4 +1,65 @@
 class Product
 {
-    
+    private List<string> _product = new List<string> {};
+    private string _productId;
+    private string _name;
+    private int _unitPrice;
+    private int _quantity;
+    // ----------------------------------------------
+    public void SetProductId(string productId)
+    {
+        _productId = productId;
+    }
+
+    public string GetProductId()
+    {
+        return _productId;
+    }
+
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetPrice(int unitPrice)
+    {
+        _unitPrice = unitPrice;
+    }
+
+    public int GetPrice()
+    {
+        return _unitPrice;
+    }
+
+    public void SetQuantity(int quantity)
+    {
+        _quantity = quantity;
+    }
+
+    public int GetQuantity()
+    {
+        return _quantity;
+    }
+    // ----------------------------------------------
+    public void MakeProduct(string productId, string name, int unitPrice, int quantity)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
