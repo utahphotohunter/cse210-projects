1e374073fc1dbbe695ff9b3def6e3e49fec78a9e
utahphotohunter <mitch.hunter1197@gmail.com>
2024-02-20 13:57:53 -0700

    finished Exercise class in Exercise program

--- final/Foundation4/Exercise.cs
+++ final/Foundation4/Exercise.cs
@@ -1,4 +1,32 @@
+using System.Runtime.InteropServices;
+
 class Exercise
 {
-    
+    private string _date;
+    private int _duration;
+//-----------------------------------------------------
+    public void SetDate(string date)
+    {
+        _date = date;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public void SetDuration(int duration)
+    {
+        _duration = duration;
+    }
+
+    public int GetDuration()
+    {
+        return _duration;
+    }
+//-----------------------------------------------------
+    public virtual string GetSummary()
+    {
+        return "";
+    }
 }
